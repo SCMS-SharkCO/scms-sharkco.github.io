ba5a417d220e165066ff91aa5726294513356cfb
Jackson5551 <45270054+Jackson5551@users.noreply.github.com>
2018-12-10 21:05:43 -0700

    Add files via upload

---
+++ javascript/Nightmode.js
@@ -0,0 +1,11 @@
+function darktheme()
+{   
+document.body.style.backgroundColor= "black";
+document.body.style.color= "white";
+}
+
+function lighttheme()
+{   
+document.body.style.backgroundColor= "white";
+document.body.style.color= "black";
+}
